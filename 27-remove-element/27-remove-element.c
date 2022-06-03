1e2e5e84a22c38795e5642c663bc973e2f016aed
Idris Saddi <47497916+idris-saddi@users.noreply.github.com>
2022-06-03 16:11:30 +0100

    Time: 0 ms (100.00%), Space: 6.1 MB (33.73%) - LeetHub

--- 27-remove-element/27-remove-element.c
+++ 27-remove-element/27-remove-element.c
@@ -1,7 +1,6 @@
 
 
 int removeElement(int* nums, int numsSize, int val){
-    int temp;
     for(int j=0;j<numsSize;j++)
         if(nums[j]==val){
             for(int i=j;i<numsSize-1;i++) nums[i]=nums[i+1];
