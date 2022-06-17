265335430a169b551b87fe93e3cdda2a675fd071
Idris Saddi <47497916+idris-saddi@users.noreply.github.com>
2022-06-17 18:46:33 +0100

    Time: 0 ms (100.00%), Space: 5.5 MB (83.33%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.c
@@ -0,0 +1,16 @@
+int isNoZero(int x){
+    if(x<1) return 0;
+    if(x<10&&x>0) return 1;
+    return(isNoZero(x/10)&&(x%10!=0));
+}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getNoZeroIntegers(int n, int* returnSize){
+    int *t=(int*)malloc(8);
+    *returnSize=2;
+    int i=1;
+    while(!(isNoZero(i)&&isNoZero(n-i))) i++;
+    t[0]=i;t[1]=n-i;
+    return t;
+}
