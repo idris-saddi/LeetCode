a0f2cb0c76ba70708c7877d741b1d6fbc02c1641
Idris Saddi <47497916+idris-saddi@users.noreply.github.com>
2022-07-25 15:24:07 +0100

    Time: 191 ms (56.48%), Space: 47.9 MB (52.27%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let s=[];
+     let i=0;
+    while(num>=1000){s[i++]='M';num-=1000;}
+    if(num>=900){s[i++]='C';s[i++]='M';num-=900;}
+    if(num>=500){s[i++]='D';num-=500;}
+    if(num>=400){s[i++]='C';s[i++]='D';num-=400;}
+    while(num>=100){s[i++]='C';num-=100;}
+    if(num>=90){s[i++]='X';s[i++]='C';num-=90;}
+    if(num>=50){s[i++]='L';num-=50;}
+    if(num>=40){s[i++]='X';s[i++]='L';num-=40;}
+    while(num>=10){s[i++]='X';num-=10;}
+    if(num>=9){s[i++]='I';s[i++]='X';num-=9;}
+    if(num>=5){s[i++]='V';num-=5;}
+    if(num>=4){s[i++]='I';s[i++]='V';num-=4;}
+    while(num>=1){s[i++]='I';num-=1;}
+ return s.join('');
+}
